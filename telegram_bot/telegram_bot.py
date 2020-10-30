1fe0d42b81d687a72b9b041900d762c8d7ada518
MalahitTV <mosbka.37@yandex.ru>
2020-10-30 09:43:18 +0300

    Fixed init func. Now __init__ expect token and user_id

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,18 +1,19 @@
 from telebot import TeleBot
 
-from .config import token, alex_id
 from .bot.bot_func import button_add
 
 
 class TelegramBot:
-    def __init__(self):
-        self.user_id = alex_id
+    def __init__(self, token: str, user_id: str):
+        self.user_id = user_id
         self.bot = TeleBot(token)
+        # current answer from bot.
+        # Can be '' or 'new_captcha'
         self.answer = None
 
         @self.bot.message_handler()
         def company_receiving(message):
-            self.bot.send_message(message.from_user.id, 'Ответ принят')
+            self.bot.send_message(message.from_user.id, 'Received')
             self.bot.stop_polling()
             self.answer = message.text
 
@@ -26,10 +27,3 @@ class TelegramBot:
         self.answer = ''
         self.bot.send_photo(self.user_id, photo=img, reply_markup=keyboard)
         self.bot.polling()
-
-
-if __name__ == '__main__':
-    telegram_bot = TelegramBot()
-    with open('screen.png', 'rb') as f:
-        img = f.read()
-    telegram_bot.send_message(img)
