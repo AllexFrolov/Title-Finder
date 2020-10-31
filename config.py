13b085d1633cff03da29708fb54e39c5c03abda6
MalahitTV <mosbka.37@yandex.ru>
2020-10-31 20:03:47 +0300

    Update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ headers = {'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                          'AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/86.0.4240.111 Safari/537.36'}
 
-proxies_url = 'http://foxtools.ru/Proxy'
+
 yandex_url = r'https://newssearch.yandex.ru/yandsearch?rpt=nnews2&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&' \
              'wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&' \
              'wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&wiz_no_news=1&' \
